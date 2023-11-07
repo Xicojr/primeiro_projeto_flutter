6bfc23b5f9a45eaca8236e44cb5c22d92dfc734d
xicojr <franciscojr96@hotmail.com>
2023-11-07 09:47:00 -0300

    entendendo funcionamento dos widgets e suas propriedades

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,35 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(color: Colors.red,width: 100,height: 100,),
-          Container(color: Colors.blue,width: 50,height: 50,),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.red,width: 100,height: 100,),
+              Container(color: Colors.blue,width: 50,height: 50,),
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blue,width: 100,height: 100,),
+              Container(color: Colors.red,width: 50,height: 50,),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyan, height: 50, width: 50,),
+              Container(color: Colors.pinkAccent, height: 50, width: 50,),
+              Container(color: Colors.purple, height: 50, width: 50,)
+            ],
+          )
         ],
-      ),
+      )
     );
   }
 }
